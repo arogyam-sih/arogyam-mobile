91e38dae49e3c6659f58423dd5746a2979dc56f1
ThEditor <theditormcpe@gmail.com>
2024-09-10 20:42:53 +0530

    feat: init

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { StatusBar, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar />
+    </View>
+  );
+}
